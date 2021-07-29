6dad693e4176c3cdf80277230977e5dea11face5
Mark Gritter <mgritter@akitasoftware.com>
2021-07-29 17:45:35 -0500

    Remove logging from tags library. (#64)

--- tags/tags.go
+++ tags/tags.go
@@ -3,16 +3,16 @@ package tags
 import (
 	"strings"
 
-	"github.com/golang/glog"
 	"github.com/google/martian/v3/tags"
 	"github.com/pkg/errors"
 )
 
 type Key = tags.Key
 
-// Returns a map from parsing a list of "key=value" pairs. Emits a warning if
-// any key in the given list is reserved. Produces an error if any element of
-// the list is improperly formatted, or if any key is given more than once.
+// Returns a map from parsing a list of "key=value" pairs.
+// Produces an error if any element of the list is improperly formatted,
+// or if any key is given more than once.
+// The caller must emit an appropriate warning if any keys are reserved.
 func FromPairs(pairs []string) (map[Key]string, error) {
 	results := make(map[Key]string, len(pairs))
 	for _, p := range pairs {
@@ -24,10 +24,6 @@ func FromPairs(pairs []string) (map[Key]string, error) {
 		k, v := Key(parts[0]), parts[1]
 		if _, ok := results[k]; ok {
 			return nil, errors.Errorf("tag with key %s specified more than once", k)
-		}
-
-		if IsReservedKey(k) {
-			glog.Warningf("%s is an Akita-reserved key. Its value may be overwritten internally", k)
 		}
 
 		results[k] = v
